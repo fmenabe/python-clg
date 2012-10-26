db8d3ba41a42ae2dd6b0237d6f42bd2adf923246
Francois Menabe <francois.menabe@unistra.fr>
2012-10-27 01:30:43 +0200

    Code stabilization

--- clg.py
+++ clg.py
@@ -10,7 +10,10 @@ class CommandLine(object):
         self.config = config
         self.subparsers = {}
 
-        self.parser = argparse.ArgumentParser()
+        add_help = False \
+            if 'main' in self.config and 'help' in self.config['main']['order'] \
+            else True
+        self.parser = argparse.ArgumentParser(add_help=add_help)
 
         for parser, params in self.config.iteritems():
             self._add_parser(parser, params)
@@ -28,15 +31,17 @@ class CommandLine(object):
 
     def _add_parser(self, parser_name, params):
         if parser_name == 'main':
+            # Main parser
             parser = self.parser
         else:
             if not hasattr(self, 'parsers'):
-                self.parsers = self.parser.add_subparsers(dest='subparsers')
+                self.parsers = self.parser.add_subparsers(dest='subparser')
             options = params['options'] if 'options' in params else {}
             parser = self.parsers.add_parser(
                 parser_name,
                 add_help=False if 'help' in options else True
             )
+            # Memorise subparser.
             self.subparsers.setdefault(parser_name, parser)
         parser._optionals.title = 'Options'
 #        parser.usage = self._format_usage(parser_name)
@@ -49,93 +54,121 @@ class CommandLine(object):
 
 
     def _add_option(self, parser, option, params):
-        option_params = [
-            "'%s'" % params['short'] if 'short' in params else '',
-            "'--%s'" % option.replace('_', '-'),
-            "dest='%s'" % option,
-            "help=u\"%s\"" % params['help'].strip()
-        ]
+        option_help = params['help'].strip()
+
+        option_str = option.replace('_', '-')
+        option = parser.add_argument(params['short'], '--%s' % option_str, dest=option) \
+            if 'short' in params \
+            else parser.add_argument('--%s' % option, dest=option)
+
+        if 'type' in params:
+            type = params['type']
+            if type == 'bool':
+                option.nargs = 0
+            elif type == 'list':
+                option.nargs = '*'
+            else:
+                option.type = type
 
         if 'default' in params:
-            option_params.append("default='%s'" % params['default'])
-            option_params[3] = option_params[3].replace(
+            option.default = params['default']
+            option_help = option_help.replace(
                 '$DEFAULT$',
                 str(params['default'])
             )
 
-        if 'type' in params:
-            option_params.append({
-                'bool': lambda: "action='store_true'",
-                'list': lambda: "nargs= '*'"
-            }.get(params['type'])())
+        if 'choices' in params:
+            option.choices = params['choices']
+            option_help = option_help.replace(
+                '$CHOICES$',
+                params['choices'].__str__()[1:-1]
+            )
 
-        # Clean option params.
-        while True:
-            try:
-                option_params.remove('')
-            except ValueError:
-                break
+        if 'required' in params:
+            option.required = True
 
-        eval("parser.add_argument(%s)" % ', '.join(option_params))
+        option.help = option_help
 
 
-    def _check_subparser(self, parser):
-        errors = []
+    def _error(self, parser, msg):
+        print self.cur_parser.format_usage()
+        print "%s: error: %s" % (self.cur_parser.prog, msg)
+        sys.exit(1)
+
+
+    def _option_str(self, parser, option):
+        option_config = self.config[parser]['options'][option]
+        option_str = ['--%s' % option.replace('_', '-')]
+        if 'short' in option_config:
+            option_str.insert(0, '-%s' % option_config['short'])
+        return "'%s'" % '/'.join(option_str)
+
+
+    def _check(self):
+        parser = self.cur_parser_name
         options = self.config[parser]['options']
-        for option, params in options.iteritems():
-            if 'required' in params and params['required']:
-                if getattr(self.args, option) is None:
-                    errors.append("option '%s' is required" % option)
+        parser_config = self.config[parser]
+        options = parser_config['options']
 
-        if 'groups' in self.config[parser]:
-            group_options = self.config[parser]['groups']
-            in_cmd = [
-                True if getattr(self.args, option) is not None else False \
-                for option in group_options
-            ]
-            nb_options = in_cmd.count(True)
-            if nb_options == 0:
-                errors.append("missing groups option")
-            elif nb_options > 1:
-                errors.append("conflict between groups options")
+        # Check conflicting options.
+        if 'groups' in parser_config:
+            groups = parser_config['groups']
+            for group, config in groups.iteritems():
+                group_options = config['options']
+                options_str = ', '.join(
+                    [self._option_str(parser, option) for option in group_options]
+                )
+                nb_options = sum(
+                    [1 for option in config['options'] if self.args[option] is not None]
+                )
+                if nb_options > 1:
+                    self._error(
+                        parser,
+                        "conflicting options: %s" % options_str
+                    )
+                if 'required' in config and config['required'] and nb_options == 0:
+                    self._error(
+                        parser,
+                        "missing one of theses options: %s" % options_str
+                    )
 
-            for option, dependancies in group_options.iteritems():
-#                print self.args
-                if getattr(self.args, option) is not None \
-                and dependancies is not None:
-                    for opt, value in dependancies.iteritems():
-                        if value and getattr(self.args, opt) is None:
-                            errors.append(
-                                "option '%s' is needed by group "
-                                "option '%s'" % (opt, option)
-                            )
-
-        return errors
+        # Check dependances between options.
+        for option, config in options.iteritems():
+            if not self.args[option]:
+                continue
+            if 'depends' not in config:
+                continue
+            for opt in config['depends']:
+                if not self.args[opt]:
+                    self._error(parser, "argument %s: missing dependance %s" % (
+                        self._option_str(parser, option),
+                        self._option_str(parser, opt)
+                    ))
 
 
     def parse(self):
-        errors = []
-        self.args = self.parser.parse_args()
+        # Get args in dictionnary (not Namespace)
+        self.args = self.parser.parse_args().__dict__
 
-#        errors.extend(self._check_subparser('main'))
-#        print self.args
-        subparser = ''
-        program = {}
-        for parser, params in self.config.iteritems():
-            if self.args.subparsers == parser:
-                subparser = parser
-                errors.extend(self._check_subparser(parser))
-                program = self.config[parser]['program']
+        self.cur_parser_name = 'main' \
+            if 'subparser' not in self.args or self.args['subparser'] is None \
+            else self.args['subparser']
+        self.cur_parser = self.parser \
+            if self.cur_parser_name == 'main' \
+            else self.subparsers[self.cur_parser_name]
 
-        if errors:
-#            print subparser, self.subparsers
-            parser = self.subparsers[subparser] \
-                if subparser != 'main' \
-                else self.parser
-            parser.print_help()
-            print "Errors:\n  * %s" % "\n  * ".join(errors)
-            sys.exit(1)
+        self._check()
 
-#        exec('import %s as module' % program['lib'])
-#        exec('module.%s(self.args.__dict__)' % program['method'])
+        parser_config = self.config[self.cur_parser_name]
+        if 'program' in parser_config and parser_config['program'] is not None:
+            program_config = parser_config['program']
+            if 'execute' in program_config:
+                # Execute code defining.
+                pass
+            elif 'module' in program_config:
+                program = program_config['module']
+                if 'path' in program:
+                    sys.path.append(program['path'])
+                exec('import %s as module' % program['lib'])
+                exec('module.%s(self.args)' % program['main'])
 
