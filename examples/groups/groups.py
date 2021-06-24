1d2997eaebe6b803c8a9feccaddfe222621d8e20
Matt McKay <mmckay@starry.com>
2021-06-24 15:57:43 -0400

    Update examples to remove warnings and ease use.
    The examples can now be run from any directory and they don't emit
    warning.

--- examples/groups/groups.py
+++ examples/groups/groups.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import clg
+import os
 import yaml
 
+CMD_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), 'groups.yml'))
 def main():
-    cmd = clg.CommandLine(yaml.load(open('groups.yml')))
+    with open(CMD_FILE) as o_f:
+        cmd = clg.CommandLine(yaml.load(o_f, Loader=yaml.SafeLoader))
     print(cmd.parse())
 
 
--- examples/subparsers/subparsers.py
+++ examples/subparsers/subparsers.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 
 import clg
+import os
 import yaml
 import yamlordereddictloader
 
+CMD_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__),'subparsers.yml'))
 def main():
-    cmd = clg.CommandLine(yaml.load(open('subparsers.yml'),
+    cmd = clg.CommandLine(yaml.load(open(CMD_FILE),
                                     Loader=yamlordereddictloader.Loader))
     print(cmd.parse())
 
